ff0f10f408a222a3ef2e5042b388854a29ba71bc
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-01-25 18:23:12 -0800

    Installed next15

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="text-3xl text-violet-400 font-black">
-      Welcome to the world of Next.js
-    </h1>
-  );
+  return <h1>Nextjs</h1>;
 }

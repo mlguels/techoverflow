feaf1747f520ce846a4779fe3a1a00890b912ae3
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-02-03 17:11:04 -0800

    Setup themes

---
+++ context/Theme.tsx
@@ -0,0 +1,12 @@
+import {
+  ThemeProviderProps,
+  ThemeProvider as NextThemeProvider,
+} from "next-themes";
+import React from "react";
+
+export default function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+}
